a356a87ffb5b751d37f501e07eb165ec91703793
JungminKo <ohohkiki@naver.com>
2022-03-15 20:39:17 +0900

    quick

--- AlgorithmLecture/Algorithms/week3/Quick.java
+++ AlgorithmLecture/Algorithms/week3/Quick.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Quick {
     private static int partition(Comparable[] a, int lo, int hi) {
         int i = lo, j = hi + 1;
@@ -24,5 +26,20 @@ public class Quick {
 
         exch(a, lo, j);
         return j;
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
     }
 }
